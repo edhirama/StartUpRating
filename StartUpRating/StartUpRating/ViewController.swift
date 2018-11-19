01e01bc275b458cf8d6c31d1dcac963cb53fb27c
Edgar Hirama <eh@ubby.io>
2018-11-19 21:01:55 -0200

    Initial commit

---
+++ StartUpRating/StartUpRating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StartUpRating
+//
+//  Created by Edgar Hirama on 19/11/18.
+//  Copyright © 2018 Edgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
